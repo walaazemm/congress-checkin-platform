01811093aaef9e4e67cf311456f0be953975daba
walaeddine <walaeddine.zemmamouche@edu.ensa.dz>
2025-11-03 02:01:56 +0100

    fix: csrf trusted origins numb

--- congress_checkin/settings.py
+++ congress_checkin/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
 
 # Always set ALLOWED_HOSTS (required by Django)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
